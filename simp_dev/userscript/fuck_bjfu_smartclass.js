f4d282be6856fc60ec1a566a01f1baf8a7ece294
xban <x_ban@qq.com>
2024-04-05 12:57:59 +0800

    1

---
+++ simp_dev/userscript/fuck_bjfu_smartclass.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name     fuck bjfu smartclass
+// @description  去掉鼠标移动检测
+// @version  1
+// @grant    none
+// @include  https://bjfu.smartclass.cn/PlayPages/Video.aspx?*
+// ==/UserScript==
+
+videoViewingDetectionSetting = 0
